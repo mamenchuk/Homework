1fe87607b132daafa3f738c76862f83ba778abf8
mamenchuk <sasamamenchuk@gmail.com>
2020-04-02 03:14:51 +0300

    home work2

---
+++ src/HomeWork/TestPerson.java
@@ -0,0 +1,15 @@
+package HomeWork;
+
+public class TestPerson {
+    public static void main(String[] args) {
+        Person Man = new Person("V1", "Vich1", );
+        Person Man2 = new Person("V2", "Vich2", );
+        Person Man3 = new Person("V3", "Vich3", );
+
+
+        System.out.println(Man.getFirstName() " " + Man.getLastName() " " +  man.getAge);
+
+
+    }
+
+    }
